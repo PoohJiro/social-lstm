791b0773c3f97ff1a475e6f338891d1eeca04023
Takauka <k22m2022@cis.twcu.ac.jp>
2025-07-30 17:35:28 +0900

    一時的な変更を削除

--- test.py
+++ test.py
@@ -142,15 +142,6 @@ def main():
             # Get the sequence
             x_seq, d_seq ,numPedsList_seq, PedsList_seq, target_id = x[0], d[0], numPedsList[0], PedsList[0], target_ids[0]
             
-            # Fix target_id if it's an array/list
-            if isinstance(target_id, (list, np.ndarray)):
-                target_id = target_id[0] if len(target_id) > 0 else target_id
-            elif hasattr(target_id, 'item'):  # PyTorch tensor
-                target_id = target_id.item()
-            
-            # Debug print
-            print(f"Processing target_id: {target_id} (type: {type(target_id)})")
-            
             dataloader.clean_test_data(x_seq, target_id, sample_args.obs_length, sample_args.pred_length)
             dataloader.clean_ped_list(x_seq, PedsList_seq, target_id, sample_args.obs_length, sample_args.pred_length)
 
