d9c9932fe4e2f5b5250157993ad9cc171ac0bed6
PoohJiro <poohjiro.0625@gmail.com>
2025-09-10 00:26:30 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,207 +1,79 @@
 import torch
 import torch.nn as nn
-import numpy as np
-
-from torch.autograd import Variable
 
 class SocialModel(nn.Module):
-
-    def __init__(self, args, infer=False):
-        '''
-        Initializer function
-        params:
-        args: Training arguments
-        infer: Training or test time (true if test time)
-        '''
+    """
+    Social-STGCNNデータセットで動作する、シンプルなEncoder-Decoder LSTMモデル
+    """
+    def __init__(self, args):
         super(SocialModel, self).__init__()
 
-        self.args = args
-        self.infer = infer
-        self.use_cuda = args.use_cuda
-
-        if infer:
-            # Test time
-            self.seq_length = 1
-        else:
-            # Training time
-            self.seq_length = args.seq_length
-
-        # Store required sizes
+        self.obs_len = args.obs_len
+        self.pred_len = args.pred_len
         self.rnn_size = args.rnn_size
-        self.grid_size = args.grid_size
         self.embedding_size = args.embedding_size
-        self.input_size = args.input_size
-        self.output_size = args.output_size
-        self.maxNumPeds=args.maxNumPeds
-        self.seq_length=args.seq_length
-        self.gru = args.gru
+        self.use_gru = args.gru
 
+        # --- Encoder ---
+        # 座標(x,y)を埋め込みベクトルに変換
+        self.input_embedding = nn.Linear(args.input_size, self.embedding_size)
+        
+        # LSTMまたはGRU層
+        if self.use_gru:
+            self.encoder = nn.GRU(self.embedding_size, self.rnn_size, batch_first=True)
+        else:
+            self.encoder = nn.LSTM(self.embedding_size, self.rnn_size, batch_first=True)
 
-        # The LSTM cell
-        self.cell = nn.LSTMCell(2*self.embedding_size, self.rnn_size)
-
-        if self.gru:
-            self.cell = nn.GRUCell(2*self.embedding_size, self.rnn_size)
-
-
-        # Linear layer to embed the input position
-        self.input_embedding_layer = nn.Linear(self.input_size, self.embedding_size)
-        # Linear layer to embed the social tensor
-        self.tensor_embedding_layer = nn.Linear(self.grid_size*self.grid_size*self.rnn_size, self.embedding_size)
-
-        # Linear layer to map the hidden state of LSTM to output
-        self.output_layer = nn.Linear(self.rnn_size, self.output_size)
-
-        # ReLU and dropout unit
+        # --- Decoder ---
+        # Decoderも同じRNNセルを使用
+        if self.use_gru:
+            self.decoder = nn.GRU(self.embedding_size, self.rnn_size, batch_first=True)
+        else:
+            self.decoder = nn.LSTM(self.embedding_size, self.rnn_size, batch_first=True)
+            
+        # RNNの出力（隠れ状態）を座標(x,y)に変換
+        self.output_layer = nn.Linear(self.rnn_size, args.output_size)
+        
+        # 活性化関数
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(args.dropout)
 
-    def getSocialTensor(self, grid, hidden_states):
-        '''
-        Computes the social tensor for a given grid mask and hidden states of all peds
-        params:
-        grid : Grid masks
-        hidden_states : Hidden states of all peds
-        '''
-        # Number of peds
-        numNodes = grid.size()[0]
+    def forward(self, obs_traj):
+        """
+        Args:
+            obs_traj (torch.Tensor): 観測された軌跡 (batch_size, obs_len, 2)
+        Returns:
+            pred_traj (torch.Tensor): 予測された軌跡 (batch_size, pred_len, 2)
+        """
+        batch_size = obs_traj.size(0)
 
-        # Construct the variable
-        social_tensor = Variable(torch.zeros(numNodes, self.grid_size*self.grid_size, self.rnn_size))
-        if self.use_cuda:
-            social_tensor = social_tensor.cuda()
+        # --- Encoding Phase ---
+        # (batch, obs_len, 2) -> (batch, obs_len, embedding_size)
+        embedded_obs = self.relu(self.input_embedding(obs_traj))
         
-        # For each ped
-        for node in range(numNodes):
-            # Compute the social tensor
-            social_tensor[node] = torch.mm(torch.t(grid[node]), hidden_states)
-
-        # Reshape the social tensor
-        social_tensor = social_tensor.view(numNodes, self.grid_size*self.grid_size*self.rnn_size)
-        return social_tensor
-            
-    #def forward(self, input_data, grids, hidden_states, cell_states ,PedsList, num_pedlist,dataloader, look_up):
-    def forward(self, *args):
-
-        '''
-        Forward pass for the model
-        params:
-        input_data: Input positions
-        grids: Grid masks
-        hidden_states: Hidden states of the peds
-        cell_states: Cell states of the peds
-        PedsList: id of peds in each frame for this sequence
-
-        returns:
-        outputs_return: Outputs corresponding to bivariate Gaussian distributions
-        hidden_states
-        cell_states
-        '''
-        # List of tensors each of shape args.maxNumPedsx3 corresponding to each frame in the sequence
-            # frame_data = tf.split(0, args.seq_length, self.input_data, name="frame_data")
-        #frame_data = [torch.squeeze(input_, [0]) for input_ in torch.split(0, self.seq_length, input_data)]
+        # エンコーダーで軌跡全体を処理し、最後の隠れ状態（と記憶セル）を取得
+        _, hidden_state = self.encoder(embedded_obs)
         
-        #print("***************************")
-        #print("input data")
-        # Construct the output variable
-        input_data = args[0]
-        grids = args[1]
-        hidden_states = args[2]
-        cell_states = args[3]
+        # --- Decoding Phase ---
+        # 予測された軌跡を格納するリスト
+        predictions = []
+        
+        # デコーダーの最初の入力は、観測の最後の座標
+        decoder_input = embedded_obs[:, -1, :].unsqueeze(1) # (batch, 1, embedding_size)
 
-        if self.gru:
-            cell_states = None
-
-        PedsList = args[4]
-        num_pedlist = args[5]
-        dataloader = args[6]
-        look_up = args[7]
-
-        numNodes = len(look_up)
-        outputs = Variable(torch.zeros(self.seq_length * numNodes, self.output_size))
-        if self.use_cuda:            
-            outputs = outputs.cuda()
-
-        # For each frame in the sequence
-        for framenum,frame in enumerate(input_data):
-
-            # Peds present in the current frame
-
-            #print("now processing: %s base frame number: %s, in-frame: %s"%(dataloader.get_test_file_name(), dataloader.frame_pointer, framenum))
-            #print("list of nodes")
-
-            #nodeIDs_boundary = num_pedlist[framenum]
-            nodeIDs = [int(nodeID) for nodeID in PedsList[framenum]]
-
-            if len(nodeIDs) == 0:
-                # If no peds, then go to the next frame
-                continue
-
-
-            # List of nodes
-            #print("lookup table :%s"% look_up)
-            list_of_nodes = [look_up[x] for x in nodeIDs]
-
-            corr_index = Variable((torch.LongTensor(list_of_nodes)))
-            if self.use_cuda:            
-                corr_index = corr_index.cuda()
-
-            #print("list of nodes: %s"%nodeIDs)
-            #print("trans: %s"%corr_index)
-            #if self.use_cuda:
-             #   list_of_nodes = list_of_nodes.cuda()
-
-
-            #print(list_of_nodes.data)
-            # Select the corresponding input positions
-            nodes_current = frame[list_of_nodes,:]
-            # Get the corresponding grid masks
-            grid_current = grids[framenum]
-
+        for _ in range(self.pred_len):
+            # デコーダーを1ステップ実行
+            output, hidden_state = self.decoder(decoder_input, hidden_state)
             
+            # 出力を座標に変換
+            # (batch, 1, rnn_size) -> (batch, 1, 2)
+            pred_step = self.output_layer(output)
+            predictions.append(pred_step)
+            
+            # 次のステップの入力は、今回予測した座標を埋め込んだもの
+            decoder_input = self.relu(self.input_embedding(pred_step))
 
+        # 予測を一つのテンソルにまとめる
+        # [(batch, 1, 2), (batch, 1, 2), ...] -> (batch, pred_len, 2)
+        pred_traj = torch.cat(predictions, dim=1)
 
-            # Get the corresponding hidden and cell states
-            hidden_states_current = torch.index_select(hidden_states, 0, corr_index)
-
-
-            if not self.gru:
-                cell_states_current = torch.index_select(cell_states, 0, corr_index)
-
-            #print(grid_current.shape)
-            #print(hidden_states_current.shape)
-            # Compute the social tensor
-            social_tensor = self.getSocialTensor(grid_current, hidden_states_current)
-
-            # Embed inputs
-            input_embedded = self.dropout(self.relu(self.input_embedding_layer(nodes_current)))
-            # Embed the social tensor
-            tensor_embedded = self.dropout(self.relu(self.tensor_embedding_layer(social_tensor)))
-
-            # Concat input
-            concat_embedded = torch.cat((input_embedded, tensor_embedded), 1)
-
-            if not self.gru:
-                # One-step of the LSTM
-                h_nodes, c_nodes = self.cell(concat_embedded, (hidden_states_current, cell_states_current))
-            else:
-                h_nodes = self.cell(concat_embedded, (hidden_states_current))
-
-
-            # Compute the output
-            outputs[framenum*numNodes + corr_index.data] = self.output_layer(h_nodes)
-
-            # Update hidden and cell states
-            hidden_states[corr_index.data] = h_nodes
-            if not self.gru:
-                cell_states[corr_index.data] = c_nodes
-
-        # Reshape outputs
-        outputs_return = Variable(torch.zeros(self.seq_length, numNodes, self.output_size))
-        if self.use_cuda:
-            outputs_return = outputs_return.cuda()
-        for framenum in range(self.seq_length):
-            for node in range(numNodes):
-                outputs_return[framenum, node, :] = outputs[framenum*numNodes + node, :]
-
-        return outputs_return, hidden_states, cell_states
+        return pred_traj
